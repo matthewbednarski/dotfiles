4f0893359fb64b4b0f94a400dd8af6d4c6ee4f4e
Matthew Bednarski <matthew.bednarski@ekr.it>
2015-02-20 20:23:07 +0100

    updated bundles.sh to use tmuxline.vim plugin

--- bundles.sh
+++ bundles.sh
@@ -14,54 +14,6 @@ git clone ssh://git@github.com/kien/ctrlp.vim
 git clone ssh://git@github.com/scrooloose/syntastic 
 
 
-#  vim-colors-solarized
-git clone git://github.com/altercation/vim-colors-solarized.git 
-
-
-#  vim-json
-git clone git://github.com/elzr/vim-json 
-
-
-#  XML-Folding
-git clone git://github.com/vim-scripts/XML-Folding 
-
-
-#  matchit
-git clone git://github.com/tmhedberg/matchit 
-
-
-#  tagbar
-git clone ssh://git@github.com/majutsushi/tagbar 
-
-
-#  vim-foldsearch
-git clone ssh://git@github.com/embear/vim-foldsearch 
-
-
-#  vim-pathogen
-git clone git://github.com/tpope/vim-pathogen.git 
-
-
-#  nerdtree
-git clone ssh://git@github.com/scrooloose/nerdtree 
-
-
-#  tcomment_vim
-git clone ssh://git@github.com/tomtom/tcomment_vim 
-
-
-#  vim-fugitive
-git clone ssh://git@github.com/tpope/vim-fugitive 
-
-
-#  vim-surround
-git clone ssh://git@github.com/tpope/vim-surround 
-
-
-#  supertab
-git clone ssh://git@github.com/ervandew/supertab 
-
-
 #  vim-airline
 git clone ssh://git@github.com/bling/vim-airline 
 
@@ -75,4 +27,56 @@ cd ~/.vim/bundle
 
 #  vim-xml
 git clone ssh://git@github.com/matthewbednarski/vim-xml 
+
+
+#  matchit
+git clone git://github.com/tmhedberg/matchit 
+
+
+#  tagbar
+git clone ssh://git@github.com/majutsushi/tagbar 
+
+
+#  vim-colors-solarized
+git clone git://github.com/altercation/vim-colors-solarized.git 
+
+
+#  vim-json
+git clone git://github.com/elzr/vim-json 
+
+
+#  XML-Folding
+git clone git://github.com/vim-scripts/XML-Folding 
+
+
+#  nerdtree
+git clone ssh://git@github.com/scrooloose/nerdtree 
+
+
+#  tcomment_vim
+git clone ssh://git@github.com/tomtom/tcomment_vim 
+
+
+#  vim-foldsearch
+git clone ssh://git@github.com/embear/vim-foldsearch 
+
+
+#  vim-pathogen
+git clone git://github.com/tpope/vim-pathogen.git 
+
+
+#  supertab
+git clone ssh://git@github.com/ervandew/supertab 
+
+
+#  tmuxline.vim
+git clone git://github.com/edkolev/tmuxline.vim 
+
+
+#  vim-fugitive
+git clone ssh://git@github.com/tpope/vim-fugitive 
+
+
+#  vim-surround
+git clone ssh://git@github.com/tpope/vim-surround 
 cd $cdold
