bb20864cffa83e1e45247253f353279b88757a82
Matthew Bednarski <matthew.bednarski@solinfo.it>
2015-08-04 15:40:34 +0200

    committing latest changes

---
+++ files/.mutt/aliases.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MESSAGE=$(cat)
+#
+# AWK='{$1=""; if( NF == 3 ) {print "alias" $0;} else if (NF == 2) '
+# AWK+='{print "alias" $0 $0;} else if (NF > 3) {print "alias", '
+# AWK+='tolower($(NF-1))"-"tolower($2) $0;}}'
+
+NEWALIAS=$(echo "${MESSAGE}" | grep ^"From: " | sed s/[\,\"\']//g | awk -c '{$1=""; if( NF == 3 ) {print "alias" $0;} else if (NF == 2) {print "alias" $0 $0;} else if (NF > 3) {print "alias", tolower($(NF-1))"-"tolower($2) $0;}}')
+
+# echo $NEWALIAS
+
+if grep -Fxq "$NEWALIAS" $HOME/.mutt/aliases.txt; then
+    :
+else
+    echo "$NEWALIAS" >> $HOME/.mutt/aliases.txt
+fi
+
+echo "${MESSAGE}"
