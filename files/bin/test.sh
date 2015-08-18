1b59b50d34b42f0a71f524b9c7c1da8c802868cf
Matthew Bednarski <matthew.bednarski@solinfo.it>
2015-08-18 11:18:37 +0200

    committing latest changes

--- files/bin/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-token=fbdf34c3a17746726b7dbea29cf36ffb211ab927
-public=true
-remote="$(echo "$public" | jq '{ public: ., files: { _: { content: "_" } } }'\
-	| curl -fnX POST -H "Authorization: token $token" -d @- https://api.github.com/gists )" 
-echo "$remote"
-echo "gisted: $(echo "$remote" | sed 's/\.git$//')"
